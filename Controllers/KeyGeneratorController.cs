56672b41e243299a94cb1e7c747275e5ded43108
Balaji <83860781+balait001@users.noreply.github.com>
2024-08-14 23:17:40 +0530

    Add files via upload

---
+++ Controllers/KeyGeneratorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KeyAPI.Model;
+
+namespace KeyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/V1/[Controller]")]
+    public class KeyGeneratorController : Controller
+    {
+        [HttpGet]
+        [Route("GetUniquekey")]
+        public IActionResult GenerateUniqueKey()
+        {
+            var uniqueKey=Guid.NewGuid().ToString();
+            var result = new UniqueKey
+            {
+                Key = uniqueKey,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            return Ok(result);
+        }
+
+         
+    }
+}
---
+++ Model/UniqueKey.cs
@@ -0,0 +1,8 @@
+﻿namespace KeyAPI.Model
+{
+    public class UniqueKey
+    {
+        public string Key { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
